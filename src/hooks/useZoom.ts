4732be55d01a7f4a13672960d174f30d1dc7589d
Jianan Zhang <zhangjianan1996@qq.com>
2021-10-07 23:39:33 +0800

    Update useZoom.ts

--- src/hooks/useZoom.ts
+++ src/hooks/useZoom.ts
@@ -14,6 +14,39 @@ const getDistance = function (start, stop) {
 	return Math.hypot(stop.x - start.x, stop.y - start.y);
 };
 
+/**
+ * 获取边框限制的transform的x, y偏移量
+ * innerDOM: 内盒子DOM
+ * outerDOM: 边框盒子DOM
+ * moveX: 盒子的x移动距离
+ * moveY: 盒子的y移动距离
+ */
+	const limitBorder = (innerDOM, outerDOM, moveX, moveY, multiple) => {
+	let { clientWidth: innerWidth, clientHeight: innerHeight, offsetLeft: innerLeft, offsetTop: innerTop } = innerDOM
+	let { clientWidth: outerWidth, clientHeight: outerHeight } = outerDOM
+	let transX
+	let transY
+	// 放大的图片超出box时 图片最多拖动到与边框对齐
+	if (innerWidth * multiple > outerWidth || innerHeight * multiple > outerHeight) {
+		if (innerWidth * multiple > outerWidth && innerWidth * multiple > outerHeight) {
+			transX = Math.min(Math.max(moveX, outerWidth - innerWidth * (multiple + 1) / 2 - innerLeft), -innerLeft + innerWidth * (multiple - 1) / 2)
+			transY = Math.min(Math.max(moveY, outerHeight - innerHeight * (multiple + 1) / 2 - innerTop), -innerTop + innerHeight * (multiple - 1) / 2)
+		} else if (innerWidth * multiple > outerWidth && !(innerWidth * multiple > outerHeight)) {
+			transX = Math.min(Math.max(moveX, outerWidth - innerWidth * (multiple + 1) / 2 - innerLeft), -innerLeft + innerWidth * (multiple - 1) / 2)
+			transY = Math.max(Math.min(moveY, outerHeight - innerHeight * (multiple + 1) / 2 - innerTop), -innerTop + innerHeight * (multiple - 1) / 2)
+		} else if (!(innerWidth * multiple > outerWidth) && innerWidth * multiple > outerHeight) {
+			transX = Math.max(Math.min(moveX, outerWidth - innerWidth * (multiple + 1) / 2 - innerLeft), -innerLeft + innerWidth * (multiple - 1) / 2)
+			transY = Math.min(Math.max(moveY, outerHeight - innerHeight * (multiple + 1) / 2 - innerTop), -innerTop + innerHeight * (multiple - 1) / 2)
+		}
+	}
+	// 图片小于box大小时 图片不能拖出边框
+	else {
+		transX = Math.max(Math.min(moveX, outerWidth - innerWidth * (multiple + 1) / 2 - innerLeft), -innerLeft + innerWidth * (multiple - 1) / 2)
+		transY = Math.max(Math.min(moveY, outerHeight - innerHeight * (multiple + 1) / 2 - innerTop), -innerTop + innerHeight * (multiple - 1) / 2)
+	}
+	return { transX, transY }
+}
+
 const useZoom = (ImgRef: HTMLImageElement) => {
 	const zoomData = useRef<ZoomData>({
 		scale: 1,
@@ -26,6 +59,8 @@ const useZoom = (ImgRef: HTMLImageElement) => {
 	});
 
 	useEffect(() => {
+		// cancel draggable
+		ImgRef.draggable = false;
 		const handleTouchStart = (event) => {
 			event.preventDefault();
 
